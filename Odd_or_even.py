c810edbdda68da9119d5749b373a20ac901cf986
Ashok375 <46166166+Ashok375@users.noreply.github.com>
2019-06-18 19:48:13 +0530

    Create Odd_or_even.py

---
+++ Odd_or_even.py
@@ -0,0 +1,6 @@
+print("Odd/Even")
+num=int(input("Enter a number to continue:"))
+if(num%2==0):
+    print("The number entered is an even number! ")
+else:
+    print("\nThe number entered is an odd number!")
