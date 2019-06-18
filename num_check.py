d9578ffb9c2923abd4030dc8192b2bf259c5b237
Ashok375 <46166166+Ashok375@users.noreply.github.com>
2019-06-18 19:54:42 +0530

    Update num_check.py

--- num_check.py
+++ num_check.py
@@ -1,9 +1,9 @@
 print("Positive/Negative/Zero")
 a=int(input("Enter a number to continue:"))
 if(a>0):
-    print("The number",a,"is a positive number!" )
+    print("Positive")
 elif(a==0):
-        print("The number entered is zero!")
+        print("Zero")
 else:
-     print("The number",a, "is a negative number!")
+     print("Negative")
    
