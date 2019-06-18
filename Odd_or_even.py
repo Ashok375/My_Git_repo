20a8f983acdb2ddbf6bdb74f7c2ab5867b7e2d4d
Ashok375 <46166166+Ashok375@users.noreply.github.com>
2019-06-18 19:56:38 +0530

    Update Odd_or_even.py

--- Odd_or_even.py
+++ Odd_or_even.py
@@ -1,6 +1,9 @@
 print("Odd/Even")
 num=int(input("Enter a number to continue:"))
 if(num%2==0):
-    print("The number entered is an even number! ")
+    print("Even")
+elif(num%2!=0):
+    print("Odd")
 else:
-    print("\nThe number entered is an odd number!")
+    print("Invalid")
+   
